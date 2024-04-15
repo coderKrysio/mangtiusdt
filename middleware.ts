6f18182032e378e8df366d9f03ca3f2dc4f18da7
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-15 20:06:24 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,9 @@ export async function middleware(request: NextRequest) {
     const { isAuthenticated } = getKindeServerSession();
     const isAuthed = await isAuthenticated();
     if (!isAuthed) {
-        return NextResponse.redirect("http://localhost:3000/api/auth/login?");
+        return NextResponse.redirect(
+            `${process.env.KINDE_SITE_URL}api/auth/login?`
+        );
     }
 }
 export const config = {
