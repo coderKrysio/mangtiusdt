56fb523f53aa946ab86f2b7653fde96200f9eaa7
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-15 20:17:36 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,10 @@ import type { NextRequest } from "next/server";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function middleware(request: NextRequest) {
-    const pathname = request.url.split(":3000")[1];
     const { isAuthenticated } = getKindeServerSession();
     const isAuthed = await isAuthenticated();
     if (!isAuthed) {
-        return NextResponse.redirect(
-            `${process.env.KINDE_SITE_URL}api/auth/login?`
-        );
+        return NextResponse.redirect(`${process.env.KINDE_SITE_LOGIN_URL}`);
     }
 }
 export const config = {
