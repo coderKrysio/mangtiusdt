8ac72a96dc3a2366d6721da290f964ad8803c36d
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-15 19:47:36 +0530

    added lib data

---
+++ lib/data.ts
@@ -0,0 +1,26 @@
+export const recent = [
+    {
+        email: "d******th@****.com",
+        date: "Mar 24, 2024",
+    },
+    {
+        email: "a******ul@****.com",
+        date: "Mar 18, 2024",
+    },
+    {
+        email: "h******in@****.com",
+        date: "Mar 2, 2024",
+    },
+    {
+        email: "t******ra@****.com",
+        date: "Feb 29, 2024",
+    },
+    {
+        email: "s******ni@****.com",
+        date: "Feb 25, 2024",
+    },
+    {
+        email: "k******na@****.com",
+        date: "Feb 22, 2024",
+    },
+];
